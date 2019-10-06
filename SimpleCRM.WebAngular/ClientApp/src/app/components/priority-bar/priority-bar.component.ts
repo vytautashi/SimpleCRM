f74ae0b0043bcb78628cdd9bf82f646878e4d68c
vytautashi <praktika2019@yahoo.com>
2019-10-06 21:28:53 +0200

    Fix priority component

--- SimpleCRM.WebAngular/ClientApp/src/app/components/priority-bar/priority-bar.component.ts
+++ SimpleCRM.WebAngular/ClientApp/src/app/components/priority-bar/priority-bar.component.ts
@@ -11,13 +11,16 @@ export class PriorityBarComponent implements OnInit {
   @Input()
   public priorityRating: number;
 
-  public ratingColor;
-  public ratingColorEmpty;
+  public ratingColor: string;
+  public ratingColorEmpty: string;
+  public ratingText: string;
   private colors = ["lightgray","green", "green", "green", "orange", "red"];
+  private priorityText = ["Lowest", "Lowest", "Low", "Normal", "Important", "Urgent"];
 
   constructor() {
   }
   ngOnInit() {
+    this.ratingText = this.priorityText[this.priorityRating];
     this.ratingColor = this.colors[this.priorityRating];
     this.ratingColorEmpty = this.colors[0];
   }
