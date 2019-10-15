44e907d3d4154fdbac9272ae06850a4906dae956
vytautashi <praktika2019@yahoo.com>
2019-10-15 13:20:47 +0200

    Implement swagger. localhost:port/swagger/

--- SimpleCRM.WebAngular/Startup.cs
+++ SimpleCRM.WebAngular/Startup.cs
@@ -8,6 +8,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SimpleCRM.Data.Context;
 using SimpleCRM.Ioc;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Reflection;
+using System.IO;
 
 namespace SimpleCRM.WebAngular
 {
@@ -19,6 +23,7 @@ namespace SimpleCRM.WebAngular
         }
 
         public IConfiguration Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,11 +38,30 @@ namespace SimpleCRM.WebAngular
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "SimpleCRM", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleCRM V1");
+                c.RoutePrefix = "swagger";
+            });
+
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
